e14560583a3f7ce8be4d2c32d8e7486e03e37bca
yudhalexa <101029025+yudhalexa@users.noreply.github.com>
2025-02-10 11:31:22 +0700

    Step 11

--- app/src/main/java/ac/id/umn/if61034_alexazetayudhadinatha_modul1/MainActivity.kt
+++ app/src/main/java/ac/id/umn/if61034_alexazetayudhadinatha_modul1/MainActivity.kt
@@ -23,6 +23,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ac.id.umn.if61034_alexazetayudhadinatha_modul1.ui.theme.BasicsCodelabTheme
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -59,6 +62,7 @@ fun OnboardingScreen(
     modifier: Modifier = Modifier
 ) {
 
+
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -72,25 +76,19 @@ fun OnboardingScreen(
             Text("Continue")
         }
     }
+
 }
 
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-//    names: List<String> = listOf("World", "Compose")
     names: List<String> = List(1000) { "$it" }
 ) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
-        items(items = names) {
-            name -> Greeting(name = name)
+        items(items = names) { name ->
+            Greeting(name = name)
         }
     }
-
-//    Column(modifier = modifier.padding(vertical = 4.dp)) {
-//        for (name in names) {
-//            Greeting(name = name)
-//        }
-//    }
 }
 
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
@@ -102,21 +100,25 @@ fun OnboardingPreview() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+private fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     var expanded by rememberSaveable { mutableStateOf(false) }
 
-    val extraPadding = if (expanded) 48.dp else 0.dp
-
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
